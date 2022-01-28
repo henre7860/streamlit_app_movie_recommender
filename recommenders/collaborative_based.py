8b4a69076c1c2ed858374b3d15e806e807730592
henre7860 <henrevandenberg@gmail.com>
2022-01-28 08:57:41 +0200

    app test

--- recommenders/collaborative_based.py
+++ recommenders/collaborative_based.py
@@ -49,7 +49,7 @@ ratings_2 = pd.read_csv('resources/data/ratings_2.csv')
 
 ratings_df = ratings_1.append(ratings_2) #.append(ratings_3).append(ratings_4)
 # Limit the dataset to 10 000 rows for initial app
-#ratings_df = ratings_df.sample(n = 10000)
+ratings_df = ratings_df.sample(n = 10000)
 #ratings_df = pd.read_csv('resources/data/ratings.csv')
 ratings_df.drop(['timestamp'], axis=1,inplace=True)
 
